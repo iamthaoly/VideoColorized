659b3fdfc020472d165a4dc7eacaa9ce880ae54e
Ly <lyttt.dev@gmail.com>
2022-07-18 15:02:33 +0700

    Initial Commit

---
+++ VideoColorized/VideoColorizedApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoColorizedApp.swift
+//  VideoColorized
+//
+//  Created by ly on 18/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct VideoColorizedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
