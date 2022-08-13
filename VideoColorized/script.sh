749173e95e8cbadfc5f1162db1ec7541146d42b8
Ly <lyttt.dev@gmail.com>
2022-08-13 16:06:07 +0700

    feat: display current installer

--- VideoColorized/script.sh
+++ VideoColorized/script.sh
@@ -23,25 +23,37 @@ install_python() {
 }
 
 install_ffmpeg() {
+    echo "----------------------"
+    echo "3. ffmpeg"
+    echo "Installing ffmpeg"
     brew install ffmpeg
 }
 
 clone_repo() {
+    echo "----------------------"
+
+    echo "Clone the repo..."
     cd $HOME
     git clone https://github.com/iamthaoly/colorized-python.git
 }
 
 install_requirements() {
+    echo "----------------------"
+    echo "Install the requirements..."
     cd colorized-python
     pip3 -m venv venv
     source venv/bin/activate
     pip3 install -r requirements.txt
     
-    pip3 install --no-deps fastai
+    pip3 install --no-deps fastai==1.0.60
 }
 
 main() {
-
+    install_brew
+    install_python
+    install_ffmpeg
+    clone_repo
+    install_requirements
     echo "Install completed!"
 }
 
