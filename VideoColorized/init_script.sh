081dba2aa5cb3a6817b3f93678d3a89372cdd818
Ly <lyttt.dev@gmail.com>
2022-08-13 18:30:43 +0700

    feat: activate venv

--- VideoColorized/init_script.sh
+++ VideoColorized/init_script.sh
@@ -6,6 +6,10 @@ clone_repo() {
     git clone https://github.com/iamthaoly/colorized-python.git
 }
 
+activate() {
+    sudo $HOME/colorized-python/venv/bin/activate
+}
+
 install_requirements() {
     echo "----------------------"
     echo "Install the requirements..."
@@ -13,7 +17,8 @@ install_requirements() {
     
     echo "Create a virtual environment"
     python3 -m venv venv
-    ./venv/bin/activate
+<!--    ./venv/bin/activate-->
+    activate
     pip3 install -r requirements.txt
     pip3 install --no-deps fastai==1.0.60
 }
