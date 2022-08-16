0a3f2b9efa2556fe592f76d59cc3010c9571d47a
Ly <lyttt.dev@gmail.com>
2022-08-16 11:06:23 +0700

    feat: Text view with multiple lines are done

--- VideoColorized/Scripts/brew_script.sh
+++ VideoColorized/Scripts/brew_script.sh
@@ -4,39 +4,45 @@ install_brew() {
     echo "----------------------"
     echo "1. Homebrew"
     if test ! $(which brew); then
-    echo "Homebrew's not installed. Installing homebrew..."
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+        echo "Homebrew's not installed. Installing homebrew..."
+        ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
     else
-    echo "Homebrew's installed."
-    echo $(brew -v)
-    echo ""
-    echo "Updating homebrew..."
-    brew update
-    
-    echo ""
+        echo "Homebrew's installed."
+        echo $(brew -v)
+        echo ""
+        echo "Updating homebrew..."
+        brew update
+        
+        echo ""
     fi
 }
 
 install_python() {
     echo "----------------------"
     echo "2. Python"
-    echo $("$BREW_PATH install python")
-    
+    brew install python
+#    res=$($BREW_PATH install python)
+#    echo "$res"
     echo $(python3 --version)
+    which python3
 }
 
 install_ffmpeg() {
     echo "----------------------"
     echo "3. ffmpeg"
     echo "Installing ffmpeg"
-    $BREW_PATH install ffmpeg
+    brew install ffmpeg
+    which ffmpeg
+    ffmpeg -version | sed -n "s/ffmpeg version \([^ ]*\).*/\1/p;"
 }
 
 install_cmake() {
     echo "----------------------"
     echo "4. cmake"
     echo "Installing cmake"
-    $BREW_PATH install cmake
+    brew install cmake
+    which cmake
+    cmake --version
 }
 
 main() {
--- VideoColorized/Views/InstallerView.swift
+++ VideoColorized/Views/InstallerView.swift
@@ -8,40 +8,61 @@
 import SwiftUI
 
 struct InstallerView: View {
+    @State var isDisplayCompletedAlert = false
 
     @ObservedObject var testManager: TestManager
 
     init() {
         self.testManager = TestManager.shared
+        testManager.terminalString =
+        """
+            1.Python Python Python Python Python Python Python Python Python Python Python Python Python Python Python Python Python Python Python
+            2.Terminal
+            3.FFMPEG
+            4.Virtualenv
+        """
     }
     
     var body: some View {
         VStack() {
             Text("Installer setup")
-            
+                .foregroundColor(.primary)
+                .font(.title3)
+            Text("We need to ")
             Button(action: {
                 print("Hello World")
                 testManager.runBrewScript()
             }) {
                 Text("Start now")
-               
             }
             
-//            VStack() {
+            Text("Installer log")
+                .padding(.top, 10.0)
+                .frame(maxWidth: .infinity, alignment: .leading)
+            VStack(alignment: .leading) {
                 ScrollView {
+                    VStack(alignment: .leading) {
                         Text(testManager.terminalString)
                             .multilineTextAlignment(.leading)
                             .fixedSize(horizontal: false, vertical: true)
-                            .background(Color.gray)
-                            .frame(minHeight: 300)
+                            .frame(minHeight: 240)
                             .frame(maxWidth: .infinity)
+                            .foregroundColor(.gray)
+                    }
                 }
-                .frame(maxWidth: .infinity)
-//            }
-//            .frame(maxWidth: .infinity, minHeight: 300.0)
+                .frame(maxWidth: .infinity, alignment: .leading)
+            }
+            .background(Color.black)
+            .alert(isPresented: $isDisplayCompletedAlert) {
+                return Alert(title: Text("Convert completed"), dismissButton: .default(Text("Okay"), action: {
+                    // Go to homescreen
+                }))
+//                                Button("OK") { }
+            }
             
         }
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .frame(maxWidth: .infinity, minHeight: 300.0, maxHeight: .infinity)
+        .padding(.all, 10.0)
     }
 }
 
